096f8d8d1fb1ee5c4a20f7d20462bbdd867a7054
muhamad shkeir <muhamad.shkeir@gmail.com>
2026-02-08 22:54:17 +0300

    disabled the COEP headers

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => {
       server: {
         port: 3000,
         host: '0.0.0.0',
+        headers: {
+          'Cross-Origin-Embedder-Policy': 'unsafe-none',
+          'Cross-Origin-Opener-Policy': 'unsafe-none'
+        }
       },
       plugins: [react()],
       define: {
